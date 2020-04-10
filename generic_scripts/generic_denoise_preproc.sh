90ed4efc2587e217e53691693ce550b069cf1866
Roopa <roopakalsankpai@gmail.com>
2020-04-10 11:12:16 +0200

    make script easier to read

--- generic_scripts/generic_denoise_preproc.sh
+++ generic_scripts/generic_denoise_preproc.sh
@@ -16,28 +16,32 @@ PE_dir=$6
 results_path="${resultsDir}/${sub}/DWI_processing"
 mkdir -p $results_path
 
+#files to be created
 dwi_raw_mif="$results_path/DWI_raw.mif"
 dwi_denoised="$results_path/DWI_denoised.mif"
 dwi_degibbsed="$results_path/DWI_degibbsed.mif"
+dwi_preprocessed="$results_path/DWI_preprocessed.mif"
 
+#PROCESSING
+
+# 1. use FSL gradient information to create DWI_raw.mif file
 mrconvert -force -fslgrad $bvec $bval $dwi_raw $dwi_raw_mif
 
-# dwi denoise
+# 2. dwi denoise
 dwidenoise $dwi_raw_mif $dwi_denoised -noise $results_path/noise_map.nii.gz -force
 
-#remove Gibbs ringing artifacts
+# 3. remove Gibbs ringing artifacts
 #mrtrix recommends running it before dwipreproc, and after dwi_denoised
 mrdegibbs -force $dwi_denoised $dwi_degibbsed
 
-# dwi preproc
-dwi_preprocessed="$results_path/DWI_preprocessed.mif"
-
+# 4. dwi preproc
 # with -rpe_none only does eddy current and motion correction
 dwipreproc -force -rpe_none -pe_dir $PE_dir -eddy_options " --data_is_shelled " $dwi_degibbsed $dwi_preprocessed
 
+# optionally remove intermediate output files
 #rm $dwi_raw_mif
 #rm $dwi_denoised
 
 
-# dwi distortion correction
+# to be implemented: dwi distortion correction
 # nonlinear reg with ANTS
