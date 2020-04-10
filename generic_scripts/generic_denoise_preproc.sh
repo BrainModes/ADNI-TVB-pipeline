7055186d08688850c97b28b39f407f5f73c56d41
Roopa <roopakalsankpai@gmail.com>
2020-04-10 11:06:10 +0200

    add "mrdegibbs" step to generic_denoise_preproc.sh

--- generic_scripts/generic_denoise_preproc.sh
+++ generic_scripts/generic_denoise_preproc.sh
@@ -1,32 +1,39 @@
-#!/bin/bash 
+#!/bin/bash
 #$ -cwd
 #$ -V
 #$ -l h_vmem=10G
 #$ -l h_rt=03:00:00
 
-# where to store data:
+# input arguments:
 resultsDir=$1
 sub=$2
-results_path="${resultsDir}/${sub}/DWI_processing"
-mkdir -p $results_path
-
-# dwi denoise
 dwi_raw=$3
 bvec=$4
 bval=$5
 PE_dir=$6
+
+#where to store output data:
+results_path="${resultsDir}/${sub}/DWI_processing"
+mkdir -p $results_path
+
 dwi_raw_mif="$results_path/DWI_raw.mif"
 dwi_denoised="$results_path/DWI_denoised.mif"
+dwi_degibbsed="$results_path/DWI_degibbsed.mif"
 
 mrconvert -force -fslgrad $bvec $bval $dwi_raw $dwi_raw_mif
 
+# dwi denoise
 dwidenoise $dwi_raw_mif $dwi_denoised -noise $results_path/noise_map.nii.gz -force
+
+#remove Gibbs ringing artifacts
+#mrtrix recommends running it before dwipreproc, and after dwi_denoised
+mrdegibbs -force $dwi_denoised $dwi_degibbsed
+
 # dwi preproc
 dwi_preprocessed="$results_path/DWI_preprocessed.mif"
 
-
 # with -rpe_none only does eddy current and motion correction
-dwipreproc -force -rpe_none -pe_dir $PE_dir -eddy_options " --data_is_shelled " $dwi_denoised $dwi_preprocessed  
+dwipreproc -force -rpe_none -pe_dir $PE_dir -eddy_options " --data_is_shelled " $dwi_degibbsed $dwi_preprocessed
 
 #rm $dwi_raw_mif
 #rm $dwi_denoised
