f69ca4af3bec8caf1e4e5c73aed49aa7cb225587
Roopa <roopakalsankpai@gmail.com>
2020-04-21 17:33:53 +0200

    add comments to generic10 script (denoise_preproc)

--- generic_scripts/generic10_denoise_preproc.sh
+++ generic_scripts/generic10_denoise_preproc.sh
@@ -23,22 +23,31 @@ dwi_degibbsed="$results_path/DWI_degibbsed.mif"
 dwi_preprocessed="$results_path/DWI_preprocessed.mif"
 
 #PROCESSING
+echo "START denoise_preproc script"
+date +"Date : %d/%m/%Y Time : %H.%M.%S"
 
 # 1. use FSL gradient information to create DWI_raw.mif file
+echo "use FSL gradient information to create DWI_raw.mif file"
 mrconvert -force -fslgrad $bvec $bval $dwi_raw $dwi_raw_mif
 
 # 2. dwi denoise
+echo "denoising DWI scan"
 dwidenoise $dwi_raw_mif $dwi_denoised -noise $results_path/noise_map.nii.gz -force
 
 # 3. remove Gibbs ringing artifacts
 #mrtrix recommends running it before dwipreproc, and after dwi_denoised
+echo "remove Gibbs ringing artifacts"
 mrdegibbs -force $dwi_denoised $dwi_degibbsed
 
 # 4. dwi preproc
 # with -rpe_none only does eddy current and motion correction
+echo "dwipreproc"
 dwipreproc -force -rpe_none -pe_dir $PE_dir -eddy_options " --data_is_shelled " $dwi_degibbsed $dwi_preprocessed
 
 # optionally remove intermediate output files
 #rm $dwi_raw_mif
 #rm $dwi_denoised
 #rm $dwi_degibbsed
+
+echo "END denoise_preproc script"
+date +"Date : %d/%m/%Y Time : %H.%M.%S"
