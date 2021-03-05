49591ccfea591dae1b568b6f04d6048cc50c37e5
Roopa <roopakalsankpai@gmail.com>
2021-03-05 12:21:31 +0100

    minor edit to log names to prevent overwrite

--- 02_after-processing_scripts/01_batch.sh
+++ 02_after-processing_scripts/01_batch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #EDIT: location where scripts are located
-sbatch --export=ALL -D ./ -o output_batch01_reorganize.txt -e error_batch01_reorganize.txt /path/to/02_after-processing_scripts/01_reorganize.py
+sbatch --export=ALL -D ./ -o output_01_reorganize.txt -e error_01_reorganize.txt /path/to/02_after-processing_scripts/01_reorganize.py
